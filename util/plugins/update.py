db1310689a454e078d113b5749d11ab0364cc15f
MarkThe <81533176+MarkThe@users.noreply.github.com>
2022-01-04 23:52:19 +0800

    Update update.py

--- util/plugins/update.py
+++ util/plugins/update.py
@@ -1,6 +1,3 @@
-# Hazard was proudly coded by Rdimo (https://github.com/Rdimo).
-# Hazard Nuker under the GNU General Public Liscense v2 (1991).
-
 import requests
 import os
 import shutil
@@ -26,14 +23,14 @@ def search_for_updates():
                     desc="Searching for updates. . .",
                     ascii=False, ncols=100):
                     sleep(0.003)
-    r = requests.get("https://github.com/Rdimo/Hazard-Nuker/releases/latest", headers=header)
+    r = requests.get("https://github.com/MarkThe/Mark-Tools/releases/latest", headers=header)
     clear()
     soup = str(BeautifulSoup(r.text, 'html.parser'))
     s1 = re.search('<title>', soup)
     s2 = re.search('·', soup)
     result_string = soup[s1.end():s2.start()]
     if THIS_VERSION not in result_string:
-        setTitle("Hazard Nuker New Update Found!")
+        setTitle("Mark-Tool New Update Found!")
         print(Style.BRIGHT + Fore.LIGHTYELLOW_EX + f'''
                 ███╗   ██╗███████╗██╗    ██╗    ██╗   ██╗██████╗ ██████╗  █████╗ ████████╗███████╗██╗
                 ████╗  ██║██╔════╝██║    ██║    ██║   ██║██╔══██╗██╔══██╗██╔══██╗╚══██╔══╝██╔════╝██║
@@ -41,35 +38,35 @@ def search_for_updates():
                 ██║╚██╗██║██╔══╝  ██║███╗██║    ██║   ██║██╔═══╝ ██║  ██║██╔══██║   ██║   ██╔══╝  ╚═╝
                 ██║ ╚████║███████╗╚███╔███╔╝    ╚██████╔╝██║     ██████╔╝██║  ██║   ██║   ███████╗██╗
                 ╚═╝  ╚═══╝╚══════╝ ╚══╝╚══╝      ╚═════╝ ╚═╝     ╚═════╝ ╚═╝  ╚═╝   ╚═╝   ╚══════╝╚═╝
-                              {Fore.LIGHTRED_EX}Looks like this Hazard Nuker {THIS_VERSION} is outdated '''.replace('█', f'{Fore.WHITE}█{Fore.RED}'), end="\n\n")
+                              {Fore.LIGHTRED_EX}Looks like this Mark-Tool {THIS_VERSION} is outdated '''.replace('█', f'{Fore.WHITE}█{Fore.RED}'), end="\n\n")
         choice = str(input(
             f'{Fore.GREEN}[{Fore.YELLOW}>>>{Fore.GREEN}] {Fore.RESET}You want to update to the latest version? (Y to update): {Fore.LIGHTRED_EX}'))
 
         if choice.upper() == 'Y':
             print(f"{Fore.WHITE}\nUpdating. . .{Fore.RESET}")
-            setTitle(f'Hazard Nuker Updating...')
-            #if they are running hazard.exe
-            if psutil.Process(os.getpid()).name() == 'HazardNuker.exe':
+            setTitle(f'Mark-Tool Updating...')
+            #if they are running Mark.exe
+            if psutil.Process(os.getpid()).name() == 'Mark.exe':
                 try:
                     soup = BeautifulSoup(requests.get("https://github.com/Rdimo/Hazard-Nuker/releases", headers=header).text, 'html.parser')
                     for link in soup.find_all('a'):
                         if "releases/download" in str(link):
                             update_url = f"https://github.com/{link.get('href')}"
                     new_version = requests.get(update_url)
-                    with open("HazardNuker.zip", 'wb')as zipfile:
+                    with open("Mark.zip", 'wb')as zipfile:
                         zipfile.write(new_version.content)
-                    with ZipFile("HazardNuker.zip", 'r') as filezip:
+                    with ZipFile("Mark.zip", 'r') as filezip:
                         filezip.extractall()
-                    os.remove("HazardNuker.zip")
-                    cwd = os.getcwd()+'\\HazardNuker\\'
+                    os.remove("Mark.zip")
+                    cwd = os.getcwd()+'\\Mark\\'
                     shutil.copyfile(cwd+'Changelog.md', 'Changelog.md')
-                    shutil.copyfile(cwd+'HazardNuker.exe', 'HazardNuker.exe')
+                    shutil.copyfile(cwd+'Mark.exe', 'Mark.exe')
                     shutil.copyfile(cwd+'README.md', 'README.md')                   
-                    shutil.rmtree('HazardNuker')
+                    shutil.rmtree('Mark')
                     setTitle('Hazard Nuker Update Complete!')
                     print(f"{Fore.GREEN}Update Successfully Finished!{Fore.RESET}")
                     sleep(1)
-                    os.startfile("HazardNuker.exe")
+                    os.startfile("Mark.exe")
                     exit()
                 except PermissionError as err:
                     clear()
@@ -78,23 +75,23 @@ def search_for_updates():
             #if they are running hazard source code
             else:
                 try:
-                    new_version = requests.get("https://github.com/Rdimo/Hazard-Nuker/archive/refs/heads/master.zip")
-                    with open("Hazard-Nuker-master.zip", 'wb')as zipfile:
+                    new_version = requests.get("https://github.com/MarkThe/Mark-Tools/archive/refs/heads/master.zip")
+                    with open("MarkThe/Mark-Tools-master.zip", 'wb')as zipfile:
                         zipfile.write(new_version.content)
-                    with ZipFile("Hazard-Nuker-master.zip", 'r') as filezip:
+                    with ZipFile("MarkThe/Mark-Tools-master.zip", 'r') as filezip:
                         filezip.extractall()
-                    os.remove("Hazard-Nuker-master.zip")
-                    cwd = os.getcwd()+'\\Hazard-Nuker-master'
+                    os.remove("MarkThe/Mark-Tools-master.zip")
+                    cwd = os.getcwd()+'\\MarkThe/Mark-Tools-master'
                     shutil.copytree(cwd, os.getcwd(), dirs_exist_ok=True)
                     shutil.rmtree(cwd)
-                    setTitle('Hazard Nuker Update Complete!')
+                    setTitle('MarkThe/Mark-Tools Update Complete!')
                     print(f"{Fore.GREEN}Update Successfully Finished!{Fore.RESET}")
                     sleep(1)
                     os.startfile("run.bat")
                     exit()
                 except Exception as err:
                     clear()
-                    print(f"{Fore.LIGHTRED_EX}An error occured while Updating Hazard Nuker-{THIS_VERSION}\n\nIf this keeps occuring try and download it manually here github.com/Rdimo/Hazard-Nuker\n\n\"{err}\"")
+                    print(f"{Fore.LIGHTRED_EX}An error occured while Updating MarkThe/Mark-Tools-{THIS_VERSION}\n\nIf this keeps occuring try and download it manually here github.com/MarkThe/Mark-Tools\n\n\"{err}\"")
                     sleep(7)
 
         else:
